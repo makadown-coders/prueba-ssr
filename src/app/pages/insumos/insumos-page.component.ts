5780437f3f534af4eab7475f3cb33c0af1133a36
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2025-02-18 12:39:20 -0800

    agregando insumo-list-skeleton

--- src/app/pages/insumos/insumos-page.component.ts
+++ src/app/pages/insumos/insumos-page.component.ts
@@ -1,10 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { InsumoListComponent } from '../../insumos/components/insumo-list/insumo-list.component';
+import { InsumoListSkeletonComponent } from './ui/insumo-list-skeleton/insumo-list-skeleton.component';
 
 @Component({
   selector: 'app-insumos-page',
-  imports: [InsumoListComponent],
+  imports: [InsumoListComponent, InsumoListSkeletonComponent],
   templateUrl: './insumos-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class InsumosPageComponent { }
+export default class InsumosPageComponent implements OnInit, OnDestroy {
+
+  //public isLoading = signal(true);
+  /*private appRef = inject(ApplicationRef);
+  private $appState = this.appRef.isStable.subscribe( isStable => {
+    console.log({isStable});
+  });*/
+
+  ngOnInit(): void {
+    /*setTimeout(() => {
+      this.isLoading.set(false);
+    }, 5000);*/
+  }
+
+  ngOnDestroy(): void {
+    /*this.$appState.unsubscribe();*/
+  }
+
+}
---
+++ src/app/pages/insumos/ui/insumo-list-skeleton/insumo-list-skeleton.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insumo-list-skeleton',
+  imports: [],
+  templateUrl: './insumo-list-skeleton.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InsumoListSkeletonComponent { }
